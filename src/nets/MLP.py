69a87f196e08756e156c3ca6a5d6de5c37a84e53
Shintaro Sakoda <sakoda@keio.jp>
2025-03-12 00:28:12 +0000

    Fixed MLP
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- src/nets/MLP.py
+++ src/nets/MLP.py
@@ -1,15 +1,16 @@
-import jax
-from typing import Callable, Any, Tuple, Iterable,Optional
+from typing import Callable, Iterable
 import flax.linen as nn
+
 
 class MLP(nn.Module):
     hiddens: Iterable[int]
     act: Callable = nn.relu
     kernel_init: Callable = nn.initializers.xavier_uniform()
     bias_init: Callable = nn.initializers.zeros
+
     @nn.compact
     def __call__(self, x):
-      for size in self.hiddens[:-1]:
-          x = nn.Dense(size)(x)
-          x = self.act(x)
-      return nn.Dense(self.hiddens[-1])(x)
+        for size in self.hiddens[:-1]:
+            x = nn.Dense(size)(x)
+            x = self.act(x)
+        return nn.Dense(self.hiddens[-1])(x)
