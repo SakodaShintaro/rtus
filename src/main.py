7063d20a9bd24fb14dc1832e553fd7877f3f6f0f
sakoda <sakoda@keio.jp>
2025-03-15 06:50:19 +0000

    Fixed implementation
    Signed-off-by: sakoda <sakoda@keio.jp>

--- src/main.py
+++ src/main.py
@@ -44,6 +44,11 @@ def bptt_grads(state, batch_x, batch_y):
     return loss, grads
 
 
+@jax.jit
+def dtanh(x):
+    return 1 - jnp.tanh(x) ** 2
+
+
 def rtrl_grads(state, batch_x, batch_y):
     params = state.params
     flat_params, unravel_fn = jax.flatten_util.ravel_pytree(params)
@@ -52,9 +57,9 @@ def rtrl_grads(state, batch_x, batch_y):
     # SimpleCellの重みサイズから隠れ状態の次元を取得
     hidden_size = params["params"]["SimpleCell_0"]["h"]["kernel"].shape[0]
 
-    Wh = params["params"]["SimpleCell_0"]["h"]["kernel"]
-    Wi = params["params"]["SimpleCell_0"]["i"]["kernel"]
-    Wi_b = params["params"]["SimpleCell_0"]["i"]["bias"]
+    R = params["params"]["SimpleCell_0"]["h"]["kernel"]
+    W = params["params"]["SimpleCell_0"]["i"]["kernel"]
+    B = params["params"]["SimpleCell_0"]["i"]["bias"]
 
     Wy = params["params"]["Dense_0"]["kernel"]
     Wy_b = params["params"]["Dense_0"]["bias"]
@@ -66,9 +71,11 @@ def rtrl_grads(state, batch_x, batch_y):
     # h(t+1) = \tanh(Wi x + bi + Wh h(t))
 
     # 感度行列 : 隠れ状態に対するパラメータの偏微分を保持 (batch, hidden_size, n_params)
-    S = jnp.zeros((batch_size, hidden_size, n_params))
+    S_W = jnp.zeros((batch_size, hidden_size, *W.shape))
+    S_R = jnp.zeros((batch_size, hidden_size, *R.shape))
 
     grad_flat = jnp.zeros(n_params)
+    grad_structured = unravel_fn(grad_flat)
     seq_len = batch_x.shape[0]
 
     loss = 0.0
@@ -81,9 +88,9 @@ def rtrl_grads(state, batch_x, batch_y):
     #     これは普通に計算できる
     # (b) dh_{t}/dW : 感度行列(S(t))
     #     これは各ステップで再帰的に計算できる
-    #     要するにh(t-1)を定数だと思って、かつS(t)をかけたものの勾配を求める
 
     for t in range(seq_len):
+        print(f"{t=}")
         x_t = batch_x[t]
         y_t_ref = batch_y[t]
 
@@ -96,11 +103,72 @@ def rtrl_grads(state, batch_x, batch_y):
         # print(f"{Wy_b.shape=}")  # (5,)
         # print(f"{y_t_ref.shape=}")  # (1, 5)
 
-        s_t = jnp.dot(x_t, Wi) + Wi_b + jnp.dot(h_t, Wh)
+        s_t = jnp.dot(x_t, W) + B + jnp.dot(h_t, R)
         h_t = jnp.tanh(s_t)
         y_t_sub = jnp.dot(h_t, Wy) + Wy_b
         loss_t = jnp.mean((y_t_sub - y_t_ref) ** 2)
         loss += loss_t
+
+        dl_dyt = 2 * (y_t_sub - y_t_ref)
+        dl_dh = jnp.dot(dl_dyt, Wy.T)
+        dl_ds = dl_dh * (1 - h_t**2)
+        print(f"{dl_ds.shape=}")
+
+        # 感度行列の更新
+        S_W = np.array(S_W)
+        S_R = np.array(S_R)
+
+        print("S_W")
+        new_S_W = np.zeros_like(S_W)
+        for k in range(hidden_size):
+            for i in range(S_W.shape[-2]):
+                for j in range(S_W.shape[-1]):
+                    new_S_W[:, k, i, j] = x_t[:, i] * (k == j)
+                    new_S_W[:, k, i, j] += np.einsum(
+                        "n,bn,bn->b", R[k], dtanh(s_t), S_W[:, :, i, j]
+                    )
+                    # for n in range(hidden_size):
+                    #     new_S_W[:, k, i, j] += (
+                    #         R[k, n] * dtanh(s_t[:, n]) * S_W[:, n, i, j]
+                    #     )
+        print("S_R")
+        new_S_R = np.zeros_like(S_R)
+        for k in range(hidden_size):
+            for i in range(S_R.shape[-2]):
+                for j in range(S_R.shape[-1]):
+                    new_S_R[:, k, i, j] = np.sum(h_t, axis=1) * (i == k)
+                    new_S_R[:, k, i, j] += np.einsum(
+                        "n,bn,bn->b", R[k], dtanh(s_t), S_R[:, :, i, j]
+                    )
+
+                    # for n in range(hidden_size):
+                    #     new_S_R[:, k, i, j] += (
+                    #         (h_t[:, n] * (i == k and j == n))
+                    #         + (R[k, n] * dtanh(s_t[:, n]) * S_R[:, n, i, j])
+                    #     )
+
+        # S_Wの更新：new_S_W[b,k,i,j] = x_t[b,i]*δ_{k,j} + Σ_n R[k,n]*dtanh(s_t[b,n])*S_W[b,n,i,j]
+        # new_S_W = jnp.einsum("bi,kj->bkij", x_t, jnp.eye(hidden_size)) + jnp.einsum(
+        #     "kn,bn,bnij->bkij", R, dtanh(s_t), S_W
+        # )
+
+        # S_Rの更新：new_S_R[b,k,i,j] = δ_{k,i}*tanh(s_t[b,j]) + Σ_n R[k,n]*dtanh(s_t[b,n])*S_R[b,n,i,j]
+        # new_S_R = jnp.eye(hidden_size)[None, :, :, None] * jnp.tanh(s_t)[
+        #     :, None, None, :
+        # ] + jnp.einsum("kn,bn,nbij->bkij", R, dtanh(s_t), S_R)
+
+        S_W = jnp.array(new_S_W)
+        S_R = jnp.array(new_S_R)
+
+        print(f"{S_W.shape=}")
+        print(f"{S_R.shape=}")
+
+        curr_grad_W = jnp.einsum("bh,bhij->bij", dl_ds, S_W)
+        curr_grad_R = jnp.einsum("bh,bhij->bij", dl_ds, S_R)
+
+        print(grad_structured["params"]["SimpleCell_0"].keys())
+        grad_structured["params"]["SimpleCell_0"]["i"]["kernel"] += curr_grad_W
+        grad_structured["params"]["SimpleCell_0"]["h"]["kernel"] += curr_grad_R
 
     # 平均損失を返す
     loss = loss / seq_len
@@ -114,7 +182,7 @@ if __name__ == "__main__":
     np.random.seed(SEED)
     rng_key = jax.random.PRNGKey(SEED)
 
-    seq_len = 10
+    seq_len = 2
     batch_size = 1
     input_dim = 5
     hidden_size = 20
@@ -149,8 +217,14 @@ if __name__ == "__main__":
     loss_diff = jnp.abs(loss_bptt - loss_rtrl)
     print(f"{loss_diff=}")
 
-    grads_bptt, _ = jax.flatten_util.ravel_pytree(grads_bptt)
-    grads_rtrl, _ = jax.flatten_util.ravel_pytree(grads_rtrl)
-    grads_diff = jnp.abs(grads_bptt - grads_rtrl)
-    grads_diff_mean = jnp.mean(grads_diff)
-    print(f"{grads_diff_mean=}")
+    grads_bptt_W = grads_bptt["params"]["SimpleCell_0"]["i"]["kernel"]
+    grads_rtrl_W = grads_rtrl["params"]["SimpleCell_0"]["i"]["kernel"]
+    grads_diff_W = jnp.abs(grads_bptt_W - grads_rtrl_W)
+    grads_diff_W = jnp.mean(grads_diff_W)
+    print(f"{grads_diff_W=}")
+
+    grads_bptt_R = grads_bptt["params"]["SimpleCell_0"]["h"]["kernel"]
+    grads_rtrl_R = grads_rtrl["params"]["SimpleCell_0"]["h"]["kernel"]
+    grads_diff_R = jnp.abs(grads_bptt_R - grads_rtrl_R)
+    grads_diff_R = jnp.mean(grads_diff_R)
+    print(f"{grads_diff_R=}")
